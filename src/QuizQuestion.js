d62a609ea0b47948ba1c5c3cac78d2ed0daa3df5
Naman Kumar <naman_89@yahoo.com>
2018-12-14 11:42:38 -0500

    Error message display

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,11 +5,19 @@ class QuizQuestion extends Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            incorrectAnswer: false
+        };
     }
 
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler();
+        }
+        else {
+            this.setState({
+                incorrectAnswer: true,
+            });
         }
     }
 
@@ -27,6 +35,10 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
+                {
+                    this.state.incorrectAnswer ?
+                        <p className='error'>Sorry, that's not right.</p> : null
+                }
             </main>
         )
     }
